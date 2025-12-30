42d0e7bfd713e393851f9a782db3dd1af9c3a10e
Anupam Lata Yadav <yanu77662@gmail.com>
2025-12-30 15:53:36 +0530

    Create 01_demo.cpp

---
+++ Part-1/01_demo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // cout << "Anupam Lata";
+     cout << "Anupam Lata" << endl;
+    // cout << "Anupam Lata" << "\n";  (use this for next line)
+
+    cout << "hello\nfrom anupam\n";
+
+    cout << "Anupam Lata \n" << "yadav" << endl;
+    return 0;
+
+}
