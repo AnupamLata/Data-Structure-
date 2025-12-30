62f3d62180300d8c8cf933df1068eb81ef5f908b
Anupam Lata Yadav <yanu77662@gmail.com>
2025-12-30 15:55:10 +0530

    Create 02_variables.cpp

---
+++ Part-1/02_variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age = 25;
+    cout << age << endl;
+    cout << sizeof(age) << endl;
+
+    char grade = 'A';
+    cout << sizeof(grade) << endl;
+
+    float PI = 3.14f;
+    cout << PI << endl;
+
+    bool isSafe = true;
+    cout << isSafe << endl;  //true -> 1  & false -> 0
+    return 0;
+}
