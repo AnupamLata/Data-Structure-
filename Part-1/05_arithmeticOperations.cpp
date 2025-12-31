8e35ff0cb5dfc958cf089dad523c71dd8c33e5ac
Anupam Lata Yadav <yanu77662@gmail.com>
2025-12-31 17:13:15 +0530

    Add arithmetic operations example in C++
    Implement basic arithmetic operations and output results.

---
+++ Part-1/05_arithmeticOperations.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 11, b = 5;
+    int sum = a+b;
+    cout << sum << endl;
+
+//or
+    cout << "sum = " << (a+b) << endl;
+    cout << "difference = " << (a-b) << endl;
+    cout << "product = " << (a*b) << endl;
+    cout << "division = " << (a/b) << endl;
+    cout << "modulo = " << (a%b) << endl;
+
+// or
+    int c = 5;
+    double d = 2;
+    cout << (5/2) << endl;
+    cout << (c/d) << endl;
+
+// or
+    cout << (5 / (double)2) << endl;
+
+
+// or
+   int ans = (5 / (double)2);
+   cout << ans << endl;
+    return 0;
+}
