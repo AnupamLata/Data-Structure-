c950d01a477ec3083aeadfe3f6b764801c63e8ae
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-02 13:53:02 +0530

    Create 01_if_else.cpp

---
+++ part_02_conditionalStatements/01_if_else.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 10;
+
+    if(n >= 0){
+        cout << "n is postive\n";
+    } else {
+        cout << "n is negative\n";
+    }
+    return 0;
+}
