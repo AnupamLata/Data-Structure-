2f5eb9dbc1464dd8b341d3f88bdb8d67a4bfefc6
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-02 13:49:34 +0530

    Add unary operators example in 09_unaryOperators.cpp
    Demonstrate increment and decrement unary operators in C++.

---
+++ Part-1/09_unaryOperators.cpp
@@ -0,0 +1,51 @@
+// increament unary operators:
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+
+    int b = a++;
+
+    cout << "b = " << b << endl;
+    cout <<"a = " << a << endl;
+
+
+     //alternate condition
+
+    int c = 12;
+    int d = ++c;
+
+    cout << "c = " << c << endl;
+    cout << "d = " << d << endl;
+
+    return 0 ;
+}
+
+// decreament unary operators:
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int a = 10;
+    int b = --a;
+
+    cout << "b = " << b << endl;
+    cout << "a = " << a << endl;
+
+    // alteranate condition
+
+    int c = 12;
+
+    int d = c--;
+
+    cout << "d = " << d << endl; 
+    cout << "c = " << c << endl; 
+
+    
+    return 0;
+
+}
+ 
