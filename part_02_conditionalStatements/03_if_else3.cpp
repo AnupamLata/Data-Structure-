c7cd72a8eb4f4a8a99971c0de2fee076514bf143
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-03 12:34:58 +0530

    Create 03_if_else3.cpp

---
+++ part_02_conditionalStatements/03_if_else3.cpp
@@ -0,0 +1,18 @@
+//check odd and even numbers
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "enter the number : ";
+    cin >> n;
+
+    if(n%2 == 0){
+        cout << "even\n";
+    } else {
+        cout << "odd\n";
+    }
+      cout << "bye\n";
+    return 0;
+}
