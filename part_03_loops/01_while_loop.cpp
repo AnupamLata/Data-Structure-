a19273550b5b26f7955b0035aa7a400e7627e3a0
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-04 20:24:01 +0530

    Refactor while loop to use 'i' for counting
    Updated while loop to use variable 'i' instead of 'count'.

---
+++ part_03_loops/01_while_loop.cpp
@@ -0,0 +1,21 @@
+// while loop: print numbers 1 to 9
+#include <iostream>
+using namespace std;
+
+int main (){
+    // int count = 1;
+
+    // while(count <= 9){
+    //     cout << count << " ";
+    //     count++;
+    // }
+    
+// or can be replace count -> i;
+    int i = 1;
+
+    while(i <= 9){
+        cout << i<< " ";
+        i++;
+    }
+    return 0 ;
+}
