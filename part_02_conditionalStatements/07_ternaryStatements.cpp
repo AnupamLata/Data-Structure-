8c7b0c7e970db9e14587f8dd102b131f417a3a7f
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-04 20:21:29 +0530

    Create 07_ternaryStatements.cpp

---
+++ part_02_conditionalStatements/07_ternaryStatements.cpp
@@ -0,0 +1,13 @@
+// ternary statement written in single line:
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "enter number : ";
+    cin >> n;
+
+    cout << (n >= 0 ? "positive" : "negative");
+    return 0;
+
+}
