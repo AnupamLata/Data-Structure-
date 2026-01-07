c5096dfa96b9d8fa3ad373be8691926d4b7558bf
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-07 22:30:45 +0530

    Create 10_quiz7.cpp

---
+++ part_03_loops/10_quiz7.cpp
@@ -0,0 +1,17 @@
+// print numbers from 1 to N
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n = 10;
+    int i = 1;
+
+    do{
+        cout << i << " ";
+        i++;
+    } while(i <= n);
+    
+    cout << endl;
+    return 0;
+}
