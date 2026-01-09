93e528fd9f7441526ad39e3ed7768a2d3a1c90ac
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-09 23:48:43 +0530

    Add nested loop to print asterisks in quiz10
    Implement nested loops to print asterisks.

---
+++ part_03_loops/14_quiz10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i=1; i<=5; i++){ // line
+
+        int m = 30;
+
+        for(int i=1; i<=m; i++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
