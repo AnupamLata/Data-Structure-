0389e1d8d960db8f5cbfd1865bf8da0291eca537
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-09 23:46:46 +0530

    Create 13_quiz9.cpp

---
+++ part_03_loops/13_quiz9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "enter number: ";
+    cin >> x;
+
+    for(int i=1; i<=x; i++){
+        cout << "* ";
+    }
+    return 0;
+
+}
