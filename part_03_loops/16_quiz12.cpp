158de26841a25686dc9c2f08973a2474273079c2
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-10 23:16:56 +0530

    Implement sum of multiples of 3 up to N
    Calculate the sum of all numbers from 1 to N that are divisible by 3.

---
+++ part_03_loops/16_quiz12.cpp
@@ -0,0 +1,22 @@
+// sum of all number from 1 to N which are divisible by 3.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "enter number: ";
+    cin >> n;
+    int sum = 0;
+
+    for(int i=1; i<=n; i++){
+        if(i%3 == 0){
+            sum += i;
+        }
+    }
+        cout << "sum = " << sum << endl;
+
+    
+    return 0;
+
+}
